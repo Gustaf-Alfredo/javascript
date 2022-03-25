45103a1d57d47cb02a45d208a3fe6b2f51ce2e35
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-03-25 17:06:57 -0300

    comentarios

---
+++ comentarios/comment.js
@@ -0,0 +1,10 @@
+alert('Olá, mundo')
+//alerta na página
+document.body.style.background = 'black' 
+//fundo da página preto
+document.querySelector('#').style.filter = 'grayscale(100%)' 
+// altera cor do elemento
+document.querySelector('#').style.visibility = 'hidden'
+// deixa invisível o elemento
+document.querySelector('span.bstn-hl-title').innerText = 'teste'
+//muda o texto selecionado
