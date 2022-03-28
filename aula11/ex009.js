724949e211bac978a3c013ef5635618128118570
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-03-28 16:41:38 -0300

    aula 9-10

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}`)
+if (país == `Brasil`) {    // se o país for Brasil: Você é Brasileiro
+    console.log('Você é Brasileiro!')
+}   else {   // se não: você é estrangeiro
+    console.log('Você é Estrangeiro!')
+}
