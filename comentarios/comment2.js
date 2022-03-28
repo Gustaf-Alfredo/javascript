0dc6135b45c5e591c4eeeb6316d2be3a36646914
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-03-28 20:04:47 -0300

    update

--- comentarios/comment2.js
+++ comentarios/comment2.js
@@ -5,3 +5,35 @@
 
 */
 
+////////////////////////////////////////////////////////////////////
+
+
+/*
+
+var agora = new Date()  //com new date e gethours vai buscar horario da maquina
+var hora = agora.getHours
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log (`Boa Tarde!`)
+} else {
+    console.log('Boa Noite!')
+}
+*/
+/*
+
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas.`)
+if (hora <= 12 && hora >= 6) {
+    console.log('Bom Dia!')
+} else if (hora <= 18 && hora > 12) {
+    console.log (`Boa Tarde!`)
+} else if (hora > 18 && hora <= 24){
+    console.log('Boa Noite!')
+} else {
+    console.log('Boa Madrugada')
+}
+
+*/
---
+++ comentarios/teste2.js
@@ -0,0 +1,12 @@
+var hora = 7
+if (hora < 5){
+console.log("boa madrugada")
+}else if(hora < 12 ){
+console.log("bom dia")
+}else if(hora == 12){
+console.log("bom meio dia")
+}else if(hora < 18){
+console.log("boa tarde")
+}else{
+    console.log("boa noite")
+}
