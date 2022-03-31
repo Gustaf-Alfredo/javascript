21fa22e01258542ff54b54bafa695358bdc1b409
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-03-30 22:00:47 -0300

    estudos

---
+++ aula13/ambiente.js
@@ -0,0 +1,32 @@
+for (var c=1; c<=10; c++) {
+    
+}
+
+//------------------------------
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+*/
+//------------------------------
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+
+//------------------------------
+
+/* 
+console.log('Paso 1')
+console.log('Paso 2')
+console.log('Paso 3')
+console.log('Paso 4')
+console.log('Paso 5')
+console.log('Paso 6')
+*/
