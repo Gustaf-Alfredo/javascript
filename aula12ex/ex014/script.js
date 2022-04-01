4d8e1be83aca0099cd019e4afae5257a3d0a2f75
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-04-01 20:31:36 -0300

    update

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,17 +1,25 @@
 function carregar() {
     let msg = window.document.getElementById('msg')
     let img = window.document.getElementById('imagem')
+    let n1 = window.document.getElementById('n1')
     let data = new Date()
-    let hora = 5
+    let hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 12 && hora < 12) {
+    if (hora >= 0 && hora < 12) {
         //BOM DIA!
-        img.src = ('imagens/Manha.png')
-    } else if (hora >= 12 && hora < 18){
+        img.src = './imagens/tratadas/Manha.png'
+        document.body.style.background = '#e2cd9f'
+        n1.innerHTML = `BOM DIA`
+        
+    } else if (hora >= 12 && hora <= 18){
         //BOA TARDE!
-        img.src = ('imagens/tarde2.png')
+        img.src = './imagens/tratadas/tarde.png'
+        document.body.style.background = '#b9846f'
+        n1.innerHTML = `BOA TARDE`
     } else {
         //BOA NOITE!
-        img.src = ('imagens/noite.png')
+        img.src = './imagens/tratadas/noite.png'
+        document.body.style.background = '#515154'
+        n1.innerHTML = `BOA NOITE`
     }
 }
