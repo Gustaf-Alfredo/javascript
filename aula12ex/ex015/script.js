61dbd818be3c59120aaedc53a3025a4ffa7bdbbe
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-04-02 02:40:23 -0300

    update

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,11 @@
+function verificar(){
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+       
+    }
+}
