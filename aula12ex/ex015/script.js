3d1d9b941a2e0bc9f84e3266ad10941230bb234d
Gustavo Alfredo <101288917+GustavoAlfredoV@users.noreply.github.com>
2022-04-03 09:40:50 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,9 +3,20 @@ function verificar(){
     let ano = data.getFullYear()
     let fano = document.getElementById('txtano')
     let res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano) {
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-       
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       //res.innerHTML = `Idade calculada: ${idade}` //teste
+       var genero = ''
+       if (fsex[0].checked){
+           genero = 'Homem'
+       } else if (fsex[1].checked) {
+           genero = 'Mulher'
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.` //teste
     }
+
 }
