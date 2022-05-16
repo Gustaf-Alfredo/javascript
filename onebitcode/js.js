ee9d326a6aea71e6477dc1768b1df1a836ac6ba1
Gustavo Alfredo <101288917+Gustaf-Alfredo@users.noreply.github.com>
2022-05-16 00:52:23 -0300

    mod6 e 7

---
+++ onebitcode/js.js
@@ -0,0 +1,7 @@
+let NomeV = window.prompt('Qual o nome do mais velho?')
+let IdadeV = window.prompt('Qual a idade do mais velho?')
+let NomeN = window.prompt('Qual o nome do mais novo?')
+let IdadeN = window.prompt('Qual a idade do mais novo?')
+
+window.alert(`${NomeV} possui ${IdadeV} ano(s)\n${NomeN} possui ${IdadeN} ano(s)\nA diferança de idade é ${Number(IdadeV - IdadeN)}`)
+
